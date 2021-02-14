409291f9d8e3f41f097a191e92d06a3d9c0b0327
ryouhama <hary0730cap@icloud.com>
2021-02-14 18:06:46 +0900

    random sort and disp cards

--- app/src/component/gameField/presenter.tsx
+++ app/src/component/gameField/presenter.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
 import { ICard } from 'type/card';
 import { Card } from 'component/card';
+import { patienceSort, randomSort} from 'util/cardManager';
+// import 'style/card.css';
 
 interface IGameFieldPresenter {
   cards: ICard[];
-}
+};
 
 export const GameFieldPresenter: React.FC<IGameFieldPresenter> = (props) => {
   const { cards } = props;
+  const style = {backgroundColor: '#008000'};
+  const tablePileStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr',
+    gridTemplateLows: '1fr auto'
+  }
+  const sortedCards = patienceSort(randomSort(cards));
+
+  const renderCardsOnTable = (index: number, cards: ICard[]) => {
+    const cardsStyle = {
+      gridRow: 2-3,
+      gridColumn: `0-${index}`
+    };
+
+    return (
+      <div key={`pile-${index}`} style={cardsStyle}>
+        {cards.map((card) => {
+          return (
+            <Card key={`${card.suit}-${card.number}`} card={card}/>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
-    <>
-      <p>this is GameField</p>
-      {cards.map((card) => {
-        return(
-          <div key={`${card.suit}-${card.number}`}>
-            <Card card={card}/>
-          </div>
-        )
-      })}
-    </>
+    <div style={style}>
+      <div className='foundation-pile'>
+        <p>foundation-pile-space</p>
+      </div>
+      <div className='deck'>
+        <p>deck-space</p>
+      </div>
+      <div className='table-pile' style={tablePileStyle}>
+        {sortedCards.map((card, index) => renderCardsOnTable(index, card))}
+      </div>
+    </div>
   );
 }
--- app/src/util/cardManager.tsx
+++ app/src/util/cardManager.tsx
@@ -1,4 +1,5 @@
-import { ICard, ISuit } from 'type/card'
+import { ICard, ISuit } from 'type/card';
+
 
 const CARD_NUMBER = 13;
 const PUBLIC_URL = `${process.env.PUBLIC_URL}`;
@@ -22,4 +23,20 @@ const getAllSuit = (): ISuit[] => {
 
 export const getCardImgPath = (card :ICard): string => {
   return `${PUBLIC_URL}/image/card/${card.suit}/${card.number}.png`
+};
+
+export const patienceSort = (cards: ICard[]): ICard[][] => {
+  var sortedArray = [];
+  for (var index=0, slicedNum=0; index < 7; cards) {
+    const slicedCards = cards.slice(slicedNum, slicedNum+index+1);
+    sortedArray.push(slicedCards);
+    slicedNum += (index+1);
+    index++;
+  }
+  return sortedArray;
+};
+
+export const randomSort = (cards: ICard[]): ICard[] => {
+  const deepCopyedCards = cards.map(card => {return {suit: card.suit, number: card.number}});
+  return deepCopyedCards.sort(() => {return Math.random() - 0.5});
 };
